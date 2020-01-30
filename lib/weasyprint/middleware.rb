736a9e488c399c0ec16a2ce3f7100f811d9e9657
bennyzr <bennyzr@yahoo.com>
2020-01-30 15:50:42 -0500

    Update middleware.rb
    Request path rewrite interferes with other pdf downloads in application.

--- lib/weasyprint/middleware.rb
+++ lib/weasyprint/middleware.rb
@@ -85,12 +85,12 @@ class WeasyPrint
     def set_request_to_render_as_pdf(env)
       @render_pdf = true
 
-      path = @request.path.sub(%r{\.pdf$}, '')
-      path = path.sub(@request.script_name, '')
+      #path = @request.path.sub(%r{\.pdf$}, '')
+      #path = path.sub(@request.script_name, '')
 
-      %w[PATH_INFO REQUEST_URI].each { |e| env[e] = path }
+      #%w[PATH_INFO REQUEST_URI].each { |e| env[e] = path }
 
-      env['HTTP_ACCEPT'] = concat(env['HTTP_ACCEPT'], Rack::Mime.mime_type('.html'))
+      #env['HTTP_ACCEPT'] = concat(env['HTTP_ACCEPT'], Rack::Mime.mime_type('.html'))
       env['Rack-Middleware-WeasyPrint'] = 'true'
     end
 
